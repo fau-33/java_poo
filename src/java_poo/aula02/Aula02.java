66c660966e852bced21147eeb5ba9b8d9bf2a641
Flávio Félix <fau_felix30@hotmail.com>
2024-08-30 16:27:23 -0300

    Colando visibilidade na classe

--- src/java_poo/aula02/Aula02.java
+++ src/java_poo/aula02/Aula02.java
@@ -4,18 +4,30 @@ public class Aula02 {
 
 	public static void main(String[] args) {
 		Caneta c1 = new Caneta();
-		c1.cor = "Azul";
-		c1.ponta = 1.0f;
+		c1.modelo = "BIC CRISTAL";
+		c1.cor = "AZUL";
+		//c1.ponta = 0.5f;
+		c1.carga = 80;
+		//c1.tampada = false;
 		c1.tampar();
 		c1.status();
 		c1.rabiscar();
 		
-		Caneta c2 = new Caneta();
-		c2.modelo = "Hostnet";
-		c2.cor = "Preta";
-		c2.destampar();
-		c2.status();
-		c2.rabiscar();
+		
+		
+		
+//		c1.cor = "Azul";
+//		c1.ponta = 1.0f;
+//		c1.tampar();
+//		c1.status();
+//		c1.rabiscar();
+//		
+//		Caneta c2 = new Caneta();
+//		c2.modelo = "Hostnet";
+//		c2.cor = "Preta";
+//		c2.destampar();
+//		c2.status();
+//		c2.rabiscar();
 
 	}
 
--- src/java_poo/aula02/Caneta.java
+++ src/java_poo/aula02/Caneta.java
@@ -1,13 +1,13 @@
 package java_poo.aula02;
 
 public class Caneta {
-	String modelo;
-	String cor;
-	float ponta;
-	int carga;
-	boolean tampada;
+	public String modelo;
+	public String cor;
+	private float ponta;
+	protected int carga;
+	private boolean tampada;
 	
-	void status() {
+	public void status() {
 		System.out.println("Modelo: " + this.modelo);
 		System.out.println("Uma caneta: " + this.cor);
 		System.out.println("Ponta: " + this.ponta);
@@ -15,17 +15,17 @@ public class Caneta {
 		System.out.println("Está tampada ? " + this.tampada);
 	}
 	
-	void rabiscar() {
+	public void rabiscar() {
 		if(this.tampada == true) {
 			System.out.println("ERRO! Não posso rabiscar!");
 		}else {
 			System.out.println("Estou rabiscando...");
 		}
 	}
-	void tampar() {
+	public void tampar() {
 		this.tampada = true;
 	}
-	void destampar() {
+	public void destampar() {
 		this.tampada = false;
 	}
 }
