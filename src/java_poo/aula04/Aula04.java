65724fe423f3368b66413e4f6f27bc7cdd35eafe
Flávio Félix <fau_felix30@hotmail.com>
2024-09-03 14:59:18 -0300

    Colocando os métodos especiais e construtores na classe caneta

---
+++ src/java_poo/aula04/Aula04.java
@@ -0,0 +1,15 @@
+package java_poo.aula04;
+
+public class Aula04 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta("VIC", "AMARELA", 0.4f);
+		c1.status();
+		
+		Caneta c2 = new Caneta("KKK", "LARANJA", 1.7f);
+		c2.status();
+		
+
+	}
+
+}
---
+++ src/java_poo/aula04/Caneta.java
@@ -0,0 +1,57 @@
+package java_poo.aula04;
+
+public class Caneta {
+	private String modelo;
+	private float ponta;
+	private String cor;
+	private boolean tampada;
+	
+	public Caneta(String modelo, String cor, float ponta) {
+		this.setModelo(modelo);
+		this.setCor(cor);
+		this.setPonta(ponta);
+		this.tampar();
+		
+
+	}
+	
+	public String getModelo() {
+		return this.modelo;
+	}
+	
+	public void setModelo(String m) {
+		this.modelo = m;
+	}
+	
+	public float getPonta() {
+		return this.ponta;
+	}
+	
+	public void setPonta(float p) {
+		this.ponta = p;
+	}
+	
+	public String getCor() {
+		return this.cor;
+	}
+	
+	public void setCor(String c) {
+		this.cor = c;
+	}
+	
+	public void tampar() {
+		this.tampada = true;
+	}
+	
+	public void destampar() {
+		this.tampada = false;
+	}
+	
+	public void status() {
+		System.out.println("SOBRE A CANETA:");
+		System.out.println("Modelo: " + this.getModelo());
+		System.out.println("Ponta: " + this.getPonta());
+		System.out.println("Cor: " + this.getCor());
+		System.out.println("Tampada: " + this.tampada);
+	}
+}
